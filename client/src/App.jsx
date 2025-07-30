eb9f362ccc3ed5e779c83d4698ec9d434b6f2692
SATYA AMAN <satyaaman1012@gmail.com>
2025-07-31 01:16:57 +0530

    Phase 0: Vite + React + Tailwind setup

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import './index.css'
+
+function App() {
+    return (
+        <div className="min-h-screen bg-gradient-to-r from-sky-400 to-blue-700 text-white flex items-center justify-center text-4xl font-bold">
+            Weather App Tailwind is Working! 🔥
+        </div>
+    );
+}
+
+
+
+export default App
