fd7df15bd81e29abbe493b852e8acf8f630e8187
SATYA AMAN <satyaaman1012@gmail.com>
2025-08-01 12:55:07 +0530

    Set up Express server with weather route and .env config for OpenWeatherMap API

---
+++ server/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const cors = require('cors');
+const weatherRoute = require('./routes/weather');
+require('dotenv').config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+app.use('/api/weather', weatherRoute);
+
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+});
